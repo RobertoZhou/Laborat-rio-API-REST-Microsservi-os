0eaa66b13409bb8a93656b416acba198c061a252
Zhou <141890618+RobertoZhou@users.noreply.github.com>
2026-08-18 21:06:15 -0300

    Etapa 08 -  Configurar e executar

--- ms-task/src/main/java/br/mytracker/mstask/TaskServiceApplication.java
+++ ms-task/src/main/java/br/mytracker/mstask/TaskServiceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsTaskApplication {
+public class TaskServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsTaskApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceApplication.class, args);
+    }
 }
